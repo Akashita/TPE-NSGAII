22fcc3924a0479e50cbc543741d61ece68a31b48
LeRagib <gabriel.vaubaillon@gmail.com>
2019-01-30 13:08:16 +0100

    je met un message parceque sinon je peux pas commit

--- single_objective_test.py
+++ single_objective_test.py
@@ -3,17 +3,15 @@ from time import time
 import inspyred
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-import math
 import numpy as np
 
-
-pop_size = 2000
+pop_size = 200
 nmb_gen = 10000
 p_crossover = 0.5
 p_mutation = 0.5
 
 
-def main(prng=None, display=True):
+def resolve_single(prng, display, problem):
 
     global pop_size
     global nmb_gen
@@ -24,7 +22,6 @@ def main(prng=None, display=True):
         prng = Random()
         prng.seed(time())
 
-    problem = inspyred.benchmarks.Ackley(2)
     ea = inspyred.ec.GA(prng)
     ea.terminator = inspyred.ec.terminators.evaluation_termination
     final_pop = ea.evolve(generator=problem.generator,
@@ -38,7 +35,7 @@ def main(prng=None, display=True):
                           num_elites=1)
 
     best = max(final_pop)
-    print('Best Solution: \n{0}'.format(str(best)))
+    #print('Best Solution: \n{0}'.format(str(best)))
 
     if display:
         graph(best)
@@ -52,9 +49,10 @@ def graph(best):
     )
 
     X, Y = np.mgrid[-5:5:0.25, -5:5:0.25]
-    Z = (-20)*np.exp((-0.2)*np.sqrt(0.5*((X**2)+(Y**2))))-np.exp(0.5*(np.cos(2*np.pi*X)+np.cos(2*np.pi*Y)))+np.exp(1)+20
 
-    ax.plot_wireframe(X, Y, Z, alpha=0.2, cmap="hot")
+    Z = (-20) * np.exp((-0.2) * np.sqrt(0.5*((X**2)+(Y**2)))) - np.exp(0.5*(np.cos(2*np.pi*X) + np.cos(2*np.pi*Y))) + np.exp(1) + 20
+
+    ax.plot_wireframe(X, Y, Z, alpha=0.2)
     #ax.plot_surface(X, Y, Z, alpha=0.5, cmap="hot")
 
     ax.set_xlabel('Variable X1')
@@ -64,4 +62,9 @@ def graph(best):
     plt.show()
 
 
-main()
+
+
+problem = inspyred.benchmarks.Ackley(dimensions = 2)
+
+
+resolve_single(None, True, problem)
