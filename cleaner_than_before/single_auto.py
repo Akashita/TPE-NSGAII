60af7c6acfab2b646b663df9b374e02bfe86bedb
LeRagib <gabriel.vaubaillon@gmail.com>
2019-04-23 12:59:03 +0200

    variation and csv files for single objectives

--- cleaner_than_before/single_auto.py
+++ cleaner_than_before/single_auto.py
@@ -1,10 +1,16 @@
 from single import *
 import time
 import inspyred
+import matplotlib.pyplot as plt
 
 problem = inspyred.benchmarks.Ackley()
 
-
+def str_nb(n,taille = 2):
+    """écrit les nombres à la bonne taille, avec des 0 devant si ils sont trops courts"""
+    ch = str(n)
+    while len(ch) < taille:
+        ch = '0' + ch
+    return ch
 
 def transform_coord(final_arc, nb_objective):
     #------------------------------------------------------------
@@ -24,11 +30,45 @@ def transform_coord(final_arc, nb_objective):
             z.append(i.fitness[2])
     return x,y,z,fit
 
+def affiche(liste):
+    for elem in liste:
+        print(elem)
 
-parameters = [200, 40, 0.5, 0.5]
-final_pop = resolve_single(problem, parameters)
-liste = [Z.fitness for Z in final_pop]
+#pop_size, nmbgen , crossover , mutation_rate
+parameters = [1000, 40, 0.5, 0.5]
+liste_soluces = []
 
+#On fait varier crossover
+for cross in range(0, 100, 10):
+    parameters[2] = cross / 100
+    print('crossover : ', cross,'\n\n')
+    minimum = [] #Liste des meilleures solutions
+    pas = 10
+    max = 1000
+
+    for param in range(0 , max , pas):
+        parameters[1] = param
+        final_pop = resolve_single(problem, parameters)
+        liste = [Z.fitness for Z in final_pop]
+        minimum.append( round( min(liste) , 3))
+        print('nmb_gen : ',param)
+
+    print('\n\n')
+    liste_soluces.append(minimum)
+print('Done')
+
+D = time.localtime()
+name = str(D[0]) + str_nb(D[1]) + str_nb(D[2]) + str_nb(D[3]) + str_nb(D[4]) + ".csv"
+f = open(name, "w")
+ch = ''
+for i in liste_soluces:
+    for j in i:
+        ch += str(j) + ','
+    ch = ch[:-1] + '\n'
+f.write(ch)
+f.close()
+
+#print(liste)
 
 #print(final_pop)
-print(min(liste),max(liste),sep='   ')
+#print(min(liste),max(liste),sep='   ')
