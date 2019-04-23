bd2e0b927ceb434db48dda8a09ebbe84a440b7b5
LeRagib <gabriel.vaubaillon@gmail.com>
2019-04-23 23:43:11 +0200

    single_auto + csv files

--- cleaner_than_before/graph_from_csv.py
+++ cleaner_than_before/graph_from_csv.py
@@ -1,19 +1,28 @@
 import matplotlib.pyplot as plt
 
-file = 'mut_04231449.csv'
+
+file = 'p_crossover_232313.csv'
+
 
 f = open(file,'r')
 lignes = f.readlines()
 f.close()
 
-pas = 3
+if lignes[0][:4] == 'Head':
+    print(lignes[0][4:])
+    lignes = lignes[1:]
+
+pas = 1
 
 for i in range(0,len(lignes),pas):
+
     ligne = lignes[i][:-1]
     ligne = ligne.split(',')
     ligne = [ float(x) for x in ligne ]
+
     liste = []
-    for j in range(0,len(ligne),10):
+    for j in range(0,len(ligne),1):
         liste.append(ligne[j])
     plt.plot(liste)
+
 plt.show()
--- cleaner_than_before/single_auto.py
+++ cleaner_than_before/single_auto.py
@@ -3,7 +3,6 @@ import time
 import inspyred
 import matplotlib.pyplot as plt
 
-problem = inspyred.benchmarks.Ackley()
 
 def str_nb(n,taille = 2):
     """écrit les nombres à la bonne taille, avec des 0 devant si ils sont trops courts"""
@@ -30,46 +29,112 @@ def transform_coord(final_arc, nb_objective):
             z.append(i.fitness[2])
     return x,y,z,fit
 
-def affiche(liste):
-    for elem in liste:
-        print(elem)
+def moyenne(L):
+    somme = 0
+    for i in L:
+        somme += i
+    return somme / len(L)
 
-#pop_size, nmbgen , crossover , mutation_rate
-#parameters = [1000, 40, 0.5, 0.5]
-parameters = [1000, 40, 0.5, 0.5]
-liste_soluces = []
-
-#On fait varier crossover
-for mut in range(0,110, 10):
-    parameters[3] = mut / 100
-    print('mutation_rate : ', parameters[3] ,'\n\n')
-    minimum = [] #Liste des meilleures solutions
-    pas = 10
-    max = 1000
-
-    for param in range(0 , max , pas):
-        parameters[1] = param
+def var_nmb_gen(parameters,min_g,max_g,pas_g):
+    minimum = []
+    for n_gen in range(min_g , max_g , pas_g):
+        parameters[1] = n_gen
+        print('nmb_gen = ',n_gen)
         final_pop = resolve_single(problem, parameters)
         liste = [Z.fitness for Z in final_pop]
-        minimum.append( round( min(liste) , 3))
-        print('nmb_gen : ',param)
+        minimum.append( round( min(liste) , 4))
+    return minimum[:]
 
-    print('\n\n')
-    liste_soluces.append(minimum[:])
-print('Done')
 
-D = time.localtime()
-name = 'mut_' + str_nb(D[1]) + str_nb(D[2]) + str_nb(D[3]) + str_nb(D[4]) + ".csv"
-f = open(name, "w")
-ch = ''
-for i in liste_soluces[:]:
-    for j in i:
-        ch += str(j) + ','
-    ch = ch[:-1] + '\n'
-f.write(ch)
-f.close()
+#On choisit le probleme pour lequel on va faire les tests :
+problem = inspyred.benchmarks.Ackley()
+name_problem = 'test' #(seulement pour le nom des fichiers)
 
-#print(liste)
+#On défini les paramètres par défaut :
+# pop_size, nmb_gen, p_crossover, p_mutation
+default_parameters = [1000, 40, 0.5, 0.5]
 
-#print(final_pop)
-#print(min(liste),max(liste),sep='   ')
+#On définie les caractéristiques des paramètres que l'on va faire varier :
+#indice : l'indice dans la liste parameters qu'il faut passer dans resolve_problem()
+#proba : si le parametre est une probabilitée ou pas (on la rentre alors en %)
+parametres = [{'name' : 'pop_size' , 'indice' : 0 , 'min' : 10 , 'max' : 100 , 'pas' : 10 , 'proba' : False},
+{'name' : 'p_crossover' , 'indice' : 2 , 'min' : 0 , 'max' : 100 , 'pas' : 10 , 'proba' : True},
+{'name' : 'p_mutation' , 'indice' : 3 , 'min' : 0 , 'max' : 100 , 'pas' : 10 , 'proba' : True}
+]
+
+#La façon dont le nombre de génération évolue :
+min_gen = 0
+max_gen = 100
+pas_gen = 10
+
+#On stocke dans exec times les temps d'exécution des différents problemes
+exec_times = []
+
+#On fait varier chaque parametre les un à la suite des autres :
+for p in parametres:
+    print('-- Variation de ',p['name'],"--")
+    #On stocke la date (pour les fichiers)
+    date = time.localtime()
+
+    #On réinitialise les paramètres :
+    parameters = default_parameters[:]
+    #On initialise la liste contenant les diverses solutions :
+    liste_soluces = []
+
+    #On commence le chronomètre :
+    start_time = time.time()
+
+    for var_param in range(p['min'] , p['max'] , p['pas']):
+
+        if p['proba']:
+            value = var_param / 100
+        else:
+            value = var_param
+
+        #On fait évoluer le paramètre :
+        parameters[p['indice']] = value
+
+        print(p['name'],' : ', value , sep='', end='\n')
+
+        #On récupère la liste des solutions optimales de chaque résolution en faisant varier
+        #le nombre de génération à chaque fois :
+        liste_soluces.append(var_nmb_gen(parameters, min_gen, max_gen, pas_gen))
+
+        print('\n'+('-'*20)+'\n')
+
+    #On arrete le chronomètre :
+    time_exec = time.time() - start_time
+    exec_times.append(time_exec)
+    print(p['name'],' done in ',time_exec,' secondes.\n',sep='')
+
+
+    # Enregistrement du fichier csv :
+
+    #On choisit le nom du fichier :
+    name = name_problem + '_'+ p['name'] + '_' + str_nb(date[2]) + str_nb(date[3]) + str_nb(date[4]) + '.csv'
+
+    #On écrit l'entête du fichier (date, temps d'exécutions, paramètres) :
+    str_date = str(date[0]) +'/'+ str_nb(date[1]) +'/'+ str_nb(date[2]) +' '+ str_nb(date[3]) +':'+ str_nb(date[4]) +':'+ str_nb(date[5])
+    entete = 'Head Début : ' + str_date
+    entete += " , Temps d'exécution : " + str(time_exec) + "secondes"
+    entete += ' , Variation de ' + p['name'] + ' entre ' + str(p['min']) + ' et ' + str(p['max'])
+    if p['proba']:
+        entete += ' (en %)'
+    entete += ' avec un pas de ' + str(p['pas'])
+    entete += ', Variation de nmb_gens de '+ str(min_gen) + ' à ' + str(max_gen)
+
+    #On écrit les données en elles mêmes :
+    ch = entete + '\n'
+    for i in liste_soluces[:]:
+        for j in i:
+            ch += str(j) + ','
+        ch = ch[:-1] + '\n'
+
+    #On enregistre le fichier :
+    f = open(name, 'w')
+    f.write(ch)
+    f.close()
+    print('---------------- CSV file saved ----------------\n\n')
+
+
+print("--- Done in",sum(exec_times),'secondes ---')
