040a93db7f224eebd3ebc4055724fcaa606b0dab
LeRagib <gabriel.vaubaillon@gmail.com>
2019-04-26 12:37:15 +0200

    ajout d'un "lissage" de courbe en plus de la moyenne sur les fichiers

--- cleaner_than_before/moyennes.py
+++ cleaner_than_before/moyennes.py
@@ -2,13 +2,16 @@ import matplotlib.pyplot as plt
 from os import system
 
 def moyenne(L):
+    if len(L) == 0:
+        print('liste vide dans moyenne')
+        return False
+
     somme = 0
     for i in L:
         somme += i
     return somme / len(L)
 
 def liste_from_csv(fichier):
-
     f = open(fichier, 'r')
     lignes = f.readlines()
     f.close()
@@ -29,43 +32,63 @@ def liste_from_csv(fichier):
         liste_totale.append(liste)
     return liste_totale
 
+def moyenne_fichier(schema):
+    cmd = 'ls ' + schema + ' > tmp.moy'
+    system(cmd)
+    f = open('tmp.moy', 'r')
+    fichiers = f.readlines()
+    f.close()
+    system('rm tmp.moy')
+    fichiers = [i[:-1] for i in fichiers]
+    nmb_fichier = len(fichiers)
 
-system('ls Ackley_p_cross*.csv > tmp.moy')
-f = open('tmp.moy', 'r')
-fichiers = f.readlines()
-f.close()
-system('rm tmp.moy')
-fichiers = [i[:-1] for i in fichiers]
-nmb_fichier = len(fichiers)
+    donnees_fichiers = []
+    for fichier in fichiers:
+        donnees_fichiers.append(liste_from_csv(fichier))
 
-donnees_fichiers = []
-for fichier in fichiers:
-    donnees_fichiers.append(liste_from_csv(fichier))
-
-nmb_lignes = min([len(x) for x in donnees_fichiers])
-nmb_val = min( [ len(donnees_fichiers[i][0]) for i in range(nmb_fichier) ] )
-
-
-new_liste = []
-for n_ligne in range(nmb_lignes):
-    new_ligne = []
-    for val in range(nmb_val):
-        new_ligne.append(moyenne([donnees_fichiers[i][n_ligne][val] for i in range(nmb_fichier)]))
-    new_liste.append(new_ligne)
+    nmb_lignes = min([len(x) for x in donnees_fichiers])
+    nmb_val = min( [ len(donnees_fichiers[i][0]) for i in range(nmb_fichier) ] )
 
 
-pas_courbes = 1
-pas_values = 1
-min_courbes = 0
+    new_liste = []
+    for n_ligne in range(nmb_lignes):
+        new_ligne = []
+        for val in range(nmb_val):
+            new_ligne.append(moyenne([donnees_fichiers[i][n_ligne][val] for i in range(nmb_fichier)]))
+        new_liste.append(new_ligne)
 
-for i in range(min_courbes,len(new_liste),pas_courbes):
-    ligne = [new_liste[i][j] for j in range(0, len(new_liste[i]), pas_values)]
-    plt.plot(ligne)
-plt.legend(range(min_courbes,len(new_liste),pas_courbes))
-plt.show()
+    return new_liste
 
+def lissage_liste(L , pas = 2):
+    new_liste = []
+    for i in range(0, len(L), pas):
+        tmp = []
+        for j in range(pas):
+            tmp.append(L[i + j])
+        new_liste.append(moyenne(tmp))
+    return new_liste
 
+def lissage_tab(tab,step = 2):
+    new_tab = []
+    for i in tab:
+        new_tab.append(lissage_liste(i,pas=step))
+    return new_tab
 
+def graph_tableau(tab,pas_courbes =1, min_courbes=0,pas_values=1):
+
+    for i in range(min_courbes,len(tab),pas_courbes):
+        ligne = [tab[i][j] for j in range(0, len(tab[i]), pas_values)]
+        plt.plot(ligne)
+    plt.legend(range(min_courbes,len(tab),pas_courbes))
+    plt.show()
+
+'Ackley_p_crossover_251532.csv'
+
+#graph_tableau(moyenne_fichier('Ackley_p_mut*.csv'),pas_courbes = 3,min_courbes = 0,pas_values = 10)
+
+#graph_tableau(lissage_tab(liste_from_csv('Ackley_p_crossover_251532.csv'),step=5),pas_values = 1)
+
+graph_tableau(lissage_tab(moyenne_fichier('Ackley_p_cross*.csv'),step=100),pas_values = 1)
 
 
 
